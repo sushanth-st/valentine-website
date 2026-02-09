8e386c4cee2ee018ce633232f5e7162654ba76f8
sushanth-st <sushanthbela@gmail.com>
2026-02-09 22:27:38 +0530

    Update site_content.js

--- site_content.js
+++ site_content.js
@@ -5,6 +5,17 @@ const SITE = {
     title: "Okay Listen to my my Appukutty 😌",
     message: "I had one job…\nAnd that was to make you smile today which I don't do normally 😄\nIf I succeed, you’ll know exactly what to do at the end 😏💖\nIf I fail… I’ll still pretend I tried 😜\nIf\nIf\nIfWEAR EARPHONES BEFORE YOU PROCEED FOR YOUR OWN GOOD😂😂"
   },
+
+  caution: {
+    message: "⚠️ Caution message goes here.\nI will update this later."
+  },
+
+  secretPage: {
+    image: "Images/image1.jpg",
+    buttonText: "Open My Heart 💖",
+    redirectUrl: "https://example.com"
+  },
+
   sections: [
     {
       title: "Funny 😄",
