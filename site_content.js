20953a8207bab9874b72af26d49cbafa2f11683f
sushanth-st <sushanthbela@gmail.com>
2026-02-09 23:29:59 +0530

    Update site_content.js

--- site_content.js
+++ site_content.js
@@ -3,11 +3,11 @@
 const SITE = {
   intro: {
     title: "Okay Listen to my my Appukutty 😌",
-    message: "I had one job…\nAnd that was to make you smile today which I don't do normally 😄\nIf I succeed, you’ll know exactly what to do at the end 😏💖\nIf I fail… I’ll still pretend I tried 🤷😃
+    message: "I had one job…\nAnd that was to make you smile today which I don't do normally 😄\nIf I succeed, you’ll know exactly what to do at the end 😏💖\nIf I fail… I’ll still pretend I tried 🤷😃" 
   },
 
   caution: {
-    message: "😜WEAR EARPHONES BEFORE YOU PROCEED FOR YOUR OWN GOOD😂😂""
+    message: "😜WEAR EARPHONES BEFORE YOU PROCEED FOR YOUR OWN GOOD😂😂"
   },
 
   secretPage: {
